ab05a0d2e49aade37fe441fb233bd0748ea6b388
Brad Richardson <bradrichardsonmail@gmail.com>
2022-07-07 22:21:40 -0400

    Black formatting

--- ecobee_config.py
+++ ecobee_config.py
@@ -18,15 +18,15 @@ class EcobeeConfig:
                 self.thermostat_ids = data.get("thermostat_ids", [])
                 self.csv_location = data.get("csv_location", "ecobee.csv")
         except:
-            logging.error("Failed to load config file, make sure to run setup (`--setup`) first!")
+            logging.error(
+                "Failed to load config file, make sure to run setup (`--setup`) first!"
+            )
             exit(1)
 
-
     def save(self):
-        with open(CONFIG_FILENAME, 'w') as config_file:
+        with open(CONFIG_FILENAME, "w") as config_file:
             json_str = json.dumps(self.__dict__, indent=4)
             config_file.write(json_str)
 
     def auth_header(self):
         return {"Authorization": "Bearer " + self.access_token}
-
